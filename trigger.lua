797356b456c74502a2830a16190805d86973fd51
Dante Dam <laggycomputer@yahoo.com>
2025-07-13 00:48:16 -0700

    trigger allows json

--- trigger.lua
+++ trigger.lua
@@ -3,6 +3,7 @@ local args = {...}
 local protocol = args[1]
 local host = args[2]
 local route = args[3] or "trigger"
+local payload = args[4] or "{}"
 
 peripheral.find("modem", rednet.open)
 local recipient = rednet.lookup(protocol, host)
@@ -10,7 +11,7 @@ assert(recipient, "no host found")
 
 assert(rednet.send(recipient, {
     route = route,
-    params = nil,
+    params = textutils.unserializeJSON(payload),
 }, protocol), "message not sent")
 
 local sender, msg, protocol = rednet.receive(protocol, 10)
