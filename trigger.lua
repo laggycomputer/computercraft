fbfa6ff0496ed8ef37f67e142fc184feb57c5d06
Dante Dam <laggycomputer@yahoo.com>
2025-07-13 01:38:53 -0700

    fix trigger json arg

--- trigger.lua
+++ trigger.lua
@@ -3,7 +3,18 @@ local args = {...}
 local protocol = args[1]
 local host = args[2]
 local route = args[3] or "trigger"
-local payload = args[4] or "{}"
+local payload = ""
+do
+    local i = 4
+    while args[i] do
+        payload = payload .. " " .. args[i]
+        i = i + 1
+    end
+end
+
+if payload == "" then
+    payload = "{}"
+end
 
 peripheral.find("modem", rednet.open)
 local recipient = rednet.lookup(protocol, host)
