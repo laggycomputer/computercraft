a91b5f3c27836fe2afc8cf52a405fa12f074ff29
Dante Dam <laggycomputer@yahoo.com>
2025-07-13 01:22:47 -0700

    install script turns on ls -a

--- install.lua
+++ install.lua
@@ -3,6 +3,8 @@ local args = {...}
 local filename = args[1] or os.getComputerLabel()
 
 os.setComputerLabel(filename)
+settings.set("list.show_hidden", true)
+settings.save()
 
 local fp = fs.open("/startup", "w+")
 fp.write('shell.run("/computercraft/' .. filename .. '")')
