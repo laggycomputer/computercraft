2bfb048ad8bef865004a40702b2f2b153fd89eb2
Dante Dam <laggycomputer@yahoo.com>
2025-07-12 20:00:08 -0700

    fix trigger script

--- trigger.lua
+++ trigger.lua
@@ -2,13 +2,17 @@ local args = {...}
 
 local protocol = args[1]
 local host = args[2]
+local route = args[3]
 
 peripheral.find("modem", rednet.open)
 local recipient = rednet.lookup(protocol, host)
 assert(recipient, "no host found")
 
-assert(rednet.send(recipient, nil, protocol), "message not sent")
+assert(rednet.send(recipient, {
+    route = route,
+    params = nil,
+}, protocol), "message not sent")
 
 local sender, msg, protocol = rednet.receive(protocol, 10)
-assert(sender, "warning: no ack recieved")
+assert(sender, "warning: nothing recieved back")
 io.write("recieved response: " .. msg .. "\n")
